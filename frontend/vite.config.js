eaf95a4cea85a8522186e0706efde8601a6d273b
pavanshetti1 <pavanshetti437@gmail.com>
2025-03-26 17:48:41 +0530

    Fix Vercel build settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(), react()],
+  base: "/",  // Ensures correct asset paths in production
+    build: {
+        outDir: "dist",  // Vercel looks for this folder
+    },
 })
